c9a8f860e4cc69d233424fccfc94786aeedfed6e
jy <junyao_92@hotmail.com>
2026-04-14 16:51:23 +0800

    remove node_modules

--- node_modules/@tanstack/start-plugin-core/dist/esm/dev-server-plugin/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-import { PluginOption } from 'vite';
-import { GetConfigFn } from '../types.js';
-export declare function devServerPlugin({ getConfig, devSsrStylesEnabled, }: {
-    getConfig: GetConfigFn;
-    devSsrStylesEnabled: boolean;
-}): PluginOption;
